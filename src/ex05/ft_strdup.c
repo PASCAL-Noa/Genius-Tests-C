4782f1a6e170b1f25c165e6682124351a6c8bdb4
PASCAL-Noa <147041489+PASCAL-Noa@users.noreply.github.com>
2026-04-02 20:07:09 +0200

    [Dev][Add] Test05

--- src/ex05/ft_strdup.c
+++ src/ex05/ft_strdup.c
@@ -1,11 +1,53 @@
-#ifdef _WIN32
-#include <io.h>
-#define write _write
-#else
-#include <unistd.h>
-#endif
+#include <stdlib.h>
 
-int main(void)
+int		ft_strlen(char *str);
+void	ft_strcpy(char *dest, char *src);
+char	*ft_strdup(char *src);
+
+/*int	main()
 {
-	return 0;
+	char	*tab;
+	int	i;
+
+	i = 0;
+	tab = ft_strdup("noa pascal");
+	while (tab[i])
+	{
+		printf("%c", tab[i], 1);
+		i++;
+	}
+}*/
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*cpy;
+	int		src_len;
+
+	src_len = ft_strlen(src) + 1;
+	cpy = (char *) malloc(src_len * sizeof(char));
+	ft_strcpy(cpy, src);
+	return (cpy);
 }
