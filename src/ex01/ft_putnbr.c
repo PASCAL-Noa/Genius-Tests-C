e02186b37c10989ab06382bfb840c2ff13d0f5ec
Sofiane-Bouhadi <sbouhadi@gaming.tech>
2026-04-10 17:51:44 +0200

    Update ft_putnbr.c

--- src/ex01/ft_putnbr.c
+++ src/ex01/ft_putnbr.c
@@ -5,3 +5,22 @@
 #include <unistd.h>
 #endif
 
+void ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	char digit = (nb % 10) + '0';
+	write(1, &digit, 1);
+}
